51032e6f554b7508a87373627fbdfcfe64cac18b
yadidro <33759273+yadidro@users.noreply.github.com>
2017-11-18 01:36:00 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -15,21 +15,58 @@ var myImage = document.querySelector('img');
 //}
 var myButton = document.querySelector('button');
 //var switch = document.querySelector('p');
-var myHeading = document.querySelector('h1');
+var myHeading = document.querySelector('h3');
 
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML = 'welcome, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'welcome, ' + storedName;
 }
 
 myButton.onclick = function() {
   setUserName();
 }
+
+const express = require('express');
+var app = express();
+var upload = require('express-fileupload');
+const http = require('http');
+http.Server(app).listen(80); // make server listen on port 80
+
+app.use(upload()); // configure middleware
+
+console.log("Server Started at port 80");
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+})
+app.post('/upload',function(req,res){
+  console.log(req.files);
+  if(req.files.upfile){
+    var file = req.files.upfile,
+      name = file.name,
+      type = file.mimetype;
+    var uploadpath = __dirname + '/uploads/' + name;
+    file.mv(uploadpath,function(err){
+      if(err){
+        console.log("File Upload Failed",name,err);
+        res.send("Error Occured!")
+      }
+      else {
+        console.log("File Uploaded",name);
+        res.send('Done! Uploading files')
+      }
+    });
+  }
+  else {
+    res.send("No File selected !");
+    res.end();
+  };
+})
